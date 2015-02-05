a177de31e564dbe0275e26ec40559fec4ad9ef6c
Roger.Cui <jesuie@live.cn>
2015-02-05 18:13:01 +0800

    add zip file

--- hfpy_code/chapter1.py
+++ hfpy_code/chapter1.py
@@ -3,33 +3,6 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
                 ["Graham Chapman", ["Michael Palin", "John Cleese",
                         "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
-print(movies)
-
-
-for each_item in movies:
-    print(each_item)
-
-
-for each_item in movies:
-    if isinstance(each_item, list):
-        for nested_item in each_item:
-            print(nested_item)
-    else:
-        print(each_item)
-
-
-for each_item in movies:
-    if isinstance(each_item, list):
-        for nested_item in each_item:
-            if isinstance(nested_item, list):
-                for deeper_item in nested_item:
-                    print(deeper_item)
-            else:
-                print(nested_item)
-    else:
-        print(each_item)
-
-
 for each_item in movies:
     if isinstance(each_item, list):
         for nested_item in each_item:
@@ -52,4 +25,6 @@ def print_lol(a_list):
             print_lol(each_item2)
         else:
             print(each_item2)
+
 print_lol(movies)
+
