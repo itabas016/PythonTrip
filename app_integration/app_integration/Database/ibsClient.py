4fe066aac8c2e48dc1af90440d7301be28d8b4f8
Roger.Cui <jesuie@live.cn>
2017-02-04 18:19:51 +0800

    add other part - tools and database and remove unused files

---
+++ app_integration/app_integration/Database/ibsClient.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import cx_Oracle
+
+CONNSTRING = 'system/S$cur1ty1@icdb'
+
+con = cx_Oracle.connect(CONNSTRING)
+print(con.version)
+
+curs = conn.cursor()
+QUERYSQL = 'select u.username, s.tablespace_name, u.created, u.account_status ' + 'from dba_users u ' + 'inner join dba_segments s on s.owner = u.username ' + 'where u.username like ''BD_%'' and s.tablespace_name like ''DATALRG%'' ' + 'group by s.tablespace_name, u.username, u.created, u.account_status ' +  'order by u.created desc;'
+curs.execute(QUERYSQL)
+con.close()
