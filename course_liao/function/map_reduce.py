16738bddaea359bd7a1e050ea5f132f0049c363d
Roger.Cui <jesuie@live.cn>
2017-08-09 18:05:02 +0800

    update reduce function exercise

--- course_liao/function/map_reduce.py
+++ course_liao/function/map_reduce.py
@@ -15,3 +15,27 @@ print(list(map(str, a)))
 print(list(map(lambda x: x *x, a)))
 print(list(map(lambda x: x *x, list(b))))
 
+#reduce(f, [x1,x2,x3,x4]) = f(f(f(x1,x2),x3),x4)
+
+def g(x, y):
+    return x+y
+
+from functools import reduce
+print(reduce(g, a))
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    return reduce(fn, map(char2num, s))
+
+print(str2int('1234'))
+
+def char2num(s):
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+
+def str2int_s(s):
+    return lambda x, y: x*10 +y, map(char2num, s)
+
+print(str2int_s('1234'))
